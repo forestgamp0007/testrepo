104adec541a1e438b36f90e66b7dec18a488163f
Leha <example@gmail.com>
2020-09-10 18:41:45 +0300

    first commit

---
+++ js/fjfrf.js
@@ -0,0 +1,52 @@
+"use sctrict";
+// const LeftBorder = 10;
+// let number = 5;
+// number = 10;
+// console.log(number);
+
+//const obj = {
+    // name: "John",
+    // age: 12,
+    // isMarried: false
+//};
+ 
+// console.log(obj.name);
+
+// let arr = ['plum.jpg',['apll.img']];
+// console.log(arr[1]);
+
+// const answer = prompt ("Вам есть 18?", "18"); 
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// // answers [0] = prompt("как ваше имя", '');
+// // answers [1] = prompt("как ваше фамилия", '');
+// // answers [2] = prompt("как ваше отчество", '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'lox';
+// alert(`hi + ${user}`);
+console.log('arr' + );
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+
+
+const isChecked = true,
+      isClose = true;
+console.log(isChecked && !isClose);
